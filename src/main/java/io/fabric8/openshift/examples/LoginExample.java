a35f55e38aaa1b942af82a31a009f97496cc6d94
Juan Vicente Herrera Ruiz de Alejo <juaherre@redhat.com>
2021-03-25 15:15:34 +0100

    Login and get version from OCP examples

---
+++ src/main/java/io/fabric8/openshift/examples/LoginExample.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.openshift.examples;
+
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.openshift.api.model.Project;
+import io.fabric8.openshift.api.model.ProjectList;
+import io.fabric8.openshift.client.OpenShiftClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginExample {
+  private static final Logger logger = LoggerFactory.getLogger(LoginExample.class);
+
+  public static void main(String[] args) {
+    try (DefaultKubernetesClient kubernetesClient = new DefaultKubernetesClient(new ConfigBuilder()
+      .withMasterUrl("https://api.YOURDOMAIN:6443")
+      .withUsername("YOURUSER")
+      .withPassword("YOURPASSSWORDORTOKEN")
+      .build())) {
+      final OpenShiftClient openShiftClient = kubernetesClient.adapt(OpenShiftClient.class);
+      logger.info("Login Successful");
+      final ProjectList pl = openShiftClient.projects().list();
+      pl.getItems().stream().map(Project::getMetadata).map(ObjectMeta::getName).forEach(logger::info);
+    }
+  }
+}
---
+++ src/main/java/io/fabric8/openshift/examples/OpenShiftVersionExample.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fabric8.openshift.examples;
+
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.VersionInfo;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.fabric8.openshift.api.model.Project;
+import io.fabric8.openshift.api.model.ProjectList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenShiftVersionExample {
+  private static final Logger logger = LoggerFactory.getLogger(OpenShiftVersionExample.class);
+
+  public static void main(String[] args) {
+    final ConfigBuilder configBuilder = new ConfigBuilder();
+    if (args.length > 0) {
+      configBuilder.withMasterUrl(args[0]);
+      configBuilder.withUsername(args[1]);
+      configBuilder.withPassword(args[2]);
+    }
+    try(OpenShiftClient client = new DefaultOpenShiftClient(configBuilder.build())) {
+      VersionInfo openShiftVersionInfo = client.getVersion();
+      logger.info("Version details of this OpenShift cluster :-");
+      logger.info("Major        : {}", openShiftVersionInfo.getMajor());
+      logger.info("Minor        : {}", openShiftVersionInfo.getMinor());
+      logger.info("GitVersion   : {}", openShiftVersionInfo.getGitVersion());
+      logger.info("BuildDate    : {}", openShiftVersionInfo.getBuildDate());
+      logger.info("GitTreeState : {}", openShiftVersionInfo.getGitTreeState());
+      logger.info("Platform     : {}", openShiftVersionInfo.getPlatform());
+      logger.info("GitVersion   : {}", openShiftVersionInfo.getGitVersion());
+      logger.info("GoVersion    : {}", openShiftVersionInfo.getGoVersion());
+      logger.info("GitCommit    : {}", openShiftVersionInfo.getGitCommit());
+    }
+  }
+}
